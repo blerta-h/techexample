128d6cde0d7cccdf205f488ad35bd2da012ff0e2
blerta-h <75258763+blerta-h@users.noreply.github.com>
2023-05-09 23:57:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ var icon = document.getElementById("icon");
 icon.onclick = function () {
     document.body.classList.toggle("dark-theme");
     if (document.body.classList.contains("dark-theme")) {
-        icon.src = "/dark theme icon/sun.png";
+        icon.src = "./dark theme icon/sun.png";
     } else {
-        icon.src = "/dark theme icon/moon.png";
+        icon.src = "./dark theme icon/moon.png";
     }
 }
 
